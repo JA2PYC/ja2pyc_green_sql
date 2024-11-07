a5b8d72c8d2a582b2ec89e383c626e9db5ed7277
gr2nade <gr2nade@gmail.com>
2024-11-07 15:24:31 +0900

    INNER OUTER JOIN

---
+++ ch04/selectOuterJoin.sql
@@ -0,0 +1,21 @@
+-- SELECT OUTER JOIN
+
+-- USE database;
+USE market_db;
+
+-- SELECT column FROM table LIMIT count;
+SELECT * FROM market_db.member LIMIT 100;
+SELECT * FROM market_db.buy LIMIT 100;
+
+-- SELECT column FROM table AS alias LEFT OUTER JOIN table AS alias
+SELECT table_member.mem_id, table_member.mem_name, table_buy.prod_name, table_member.addr
+	FROM market_db.member AS table_member 
+    LEFT OUTER JOIN market_db.buy AS table_buy	
+    ON table_member.mem_id = table_buy.mem_id 
+    ORDER BY table_member.mem_id;
+
+SELECT table_member.mem_id, table_member.mem_name, table_buy.prod_name, table_member.addr 
+	FROM market_db.member AS table_member
+    INNER JOIN market_db.buy AS table_buy 
+    ON table_member.mem_id = table_buy.mem_id 
+    ORDER BY table_member.mem_id;
---
+++ ch99/createDatabaseUserGrant.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE database;
+CREATE DATABASE ja2pyc_wp;
+
+-- SHOW DATABASES;
+SHOW DATABASES;
+
+-- USE database;
+USE ja2pyc_wp;
+USE mysql;
+
+-- SHOW TABLES:
+SHOW TABLES;
+
+-- SELECT column FROM table LIMIT count;
+SELECT * FROM mysql.user LIMIT 100;
